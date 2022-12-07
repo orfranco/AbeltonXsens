ee6d53dc80ccbcf0b59f4d9a64df3a952f22ec08
orfranco <or.franco@mail.huji.ac.il>
2022-12-07 14:23:39 +0200

    changed values wheel to act sin function - without values jumping

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -11,23 +11,23 @@
 
 const struct XsensParameter AbletonXsensAudioProcessor::params[] =
 {
-        {"euler_x",-180,180},
-        {"euler_y",-180,180},
-        {"euler_z",-180,180},
-       /* {"acc_x",-180,180},
-        {"acc_y",-180,180},
-        {"acc_z",-180,180},
-        {"gyr_x",-2000,2000},
-        {"gyr_y",-2000,2000},
-        {"gyr_z",-2000,2000},
-        //TODO: validate min-max values:
-        {"mag_x",-10,10},
-        {"mag_y",-10,10},
-        {"mag_z",-10,100},
-        {"quaternion_w",-180,180},
-        {"quaternion_x",-180,180},
-        {"quaternion_y",-180,180},
-        {"quaternion_z",-180,180} */
+        {"euler_x",-1, 1},
+        {"euler_y",-1, 1},
+        {"euler_z",-1, 1},
+        /* {"acc_x",-180,180},
+         {"acc_y",-180,180},
+         {"acc_z",-180,180},
+         {"gyr_x",-2000,2000},
+         {"gyr_y",-2000,2000},
+         {"gyr_z",-2000,2000},
+         //TODO: validate min-max values:
+         {"mag_x",-10,10},
+         {"mag_y",-10,10},
+         {"mag_z",-10,100},
+         {"quaternion_w",-180,180},
+         {"quaternion_x",-180,180},
+         {"quaternion_y",-180,180},
+         {"quaternion_z",-180,180} */
 };
 //==============================================================================
 
@@ -41,12 +41,12 @@ AbletonXsensAudioProcessor::AbletonXsensAudioProcessor()
         .withOutput("Output", juce::AudioChannelSet::stereo(), true)
 #endif
     ), treeState(*this, nullptr, "PARAMETER", createParameters()),
-        startTime(juce::Time::getMillisecondCounterHiRes() * 0.001),
-        m_log_file("~/log_test.txt"), m_logger(m_log_file, "Welcome to the log", 0),
-        streamAllocator(MAX_SENSORS_NUM)
+    startTime(juce::Time::getMillisecondCounterHiRes() * 0.001),
+    m_log_file("~/log_test.txt"), m_logger(m_log_file, "Welcome to the log", 0),
+    streamAllocator(MAX_SENSORS_NUM)
 
 #endif
-    
+
 {
     juce::Logger::setCurrentLogger(&m_logger);
     std::function<void(std::string)> const& handler = std::bind(&AbletonXsensAudioProcessor::onDataTransfer, this, std::placeholders::_1);
@@ -66,14 +66,19 @@ juce::AudioProcessorValueTreeState::ParameterLayout AbletonXsensAudioProcessor::
             auto name = param.name + "_" + std::to_string(sensorIdx);
             pluginParams.push_back(
                 std::make_unique<juce::AudioParameterFloat>(name, name, param.minValue, param.maxValue, (param.minValue + param.maxValue) / 2));
-            
+
             auto currSensitivityName = param.name + "_" + std::to_string(sensorIdx) + SENSITIVITY;
             pluginParams.push_back(
                 std::make_unique<juce::AudioParameterFloat>(currSensitivityName, currSensitivityName, MIN_SENSITIVITY, MAX_SENSITIVITY, 1));
-            
+
             auto currInvertionName = param.name + "_" + std::to_string(sensorIdx) + INVERTION;
             pluginParams.push_back(
                 std::make_unique<juce::AudioParameterBool>(currInvertionName, currInvertionName, false, currInvertionName));
+
+            auto currShiftingName = param.name + "_" + std::to_string(sensorIdx) + SHIFTING;
+            pluginParams.push_back(
+                std::make_unique<juce::AudioParameterFloat>(currShiftingName, currShiftingName, MIN_SHIFTING, MAX_SHIFTING, 0));
+
         }
     }
 
@@ -96,7 +101,7 @@ void AbletonXsensAudioProcessor::onDataTransfer(std::string msg) {
     this->extractSensorData(stream, currentLine, buffer, logMessage, currSlot);
 }
 
-/** 
+/**
 this method extracts the mac-address of the sensor.
 it then uses the streamAllocator to get the slot.
 */
@@ -119,25 +124,15 @@ void AbletonXsensAudioProcessor::extractSensorData(std::istringstream& stream, s
         // if found:
         if (pos != std::string::npos)
         {
-            std::string currParam = currentLine.substr(0, pos)+"_"+std::to_string(currSlot);
+            std::string currParam = currentLine.substr(0, pos) + "_" + std::to_string(currSlot);
             if (this->treeState.getParameter(currParam) != nullptr) {
                 //validate that value isn't NaN. TODO: check if it works.
                 if (currentLine.find("NaN", pos) == std::string::npos) {
                     double paramValue = std::stod(currentLine.substr(pos + 1, buffer));
-                    //XsensSliders[currParam]->slider.setValue(XsensSliders[currParam]->sensitivity * XsensSliders[currParam]->invertion * paramValue);
                     if (this->treeState.getParameter(currParam + SENSITIVITY) != nullptr && this->treeState.getParameter(currParam + INVERTION) != nullptr) {
-                        auto paramSensitivity = this->treeState.getRawParameterValue(currParam + SENSITIVITY);
-                        auto paramInvertion = this->treeState.getParameter(currParam + INVERTION);
-                        float newValue;
-                        if ((bool)paramInvertion->getValue() == false) {
-                            newValue = paramValue * *paramSensitivity;
-                        }
-                        else {
-                            newValue = -1 * paramValue * *paramSensitivity;
-                        }
+                        float newValue = mapSensorValues(currParam, paramValue);
                         this->treeState.getParameter(currParam)->setValueNotifyingHost(
                             this->treeState.getParameterRange(currParam).convertTo0to1(newValue));
-
                         logMessage << currParam << ": " << paramValue << "\n";
                     }
                 }
@@ -150,6 +145,21 @@ void AbletonXsensAudioProcessor::extractSensorData(std::istringstream& stream, s
     juce::Logger::getCurrentLogger()->writeToLog(logMessage);
 }
 
+/**
+    this methods maps a sensor parameter value to a new value that is generated by sin function and 
+    shifting/sensitivity/invertion params.
+*/
+float AbletonXsensAudioProcessor::mapSensorValues(std::string currParam, double paramValue) {
+    auto paramSensitivity = this->treeState.getRawParameterValue(currParam + SENSITIVITY);
+    auto paramShifting = this->treeState.getRawParameterValue(currParam + SHIFTING);
+    auto paramInvertion = this->treeState.getParameter(currParam + INVERTION);
+    int sign = 1;
+    if ((bool)paramInvertion->getValue() == true) {
+        sign = -1;
+    }
+    return sign * std::sin(((paramValue * *paramSensitivity / 180) * 3.14) + *paramShifting);
+}
+
 //==============================================================================
 const juce::String AbletonXsensAudioProcessor::getName() const
 {
@@ -158,29 +168,29 @@ const juce::String AbletonXsensAudioProcessor::getName() const
 
 bool AbletonXsensAudioProcessor::acceptsMidi() const
 {
-   #if JucePlugin_WantsMidiInput
+#if JucePlugin_WantsMidiInput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 bool AbletonXsensAudioProcessor::producesMidi() const
 {
-   #if JucePlugin_ProducesMidiOutput
+#if JucePlugin_ProducesMidiOutput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 bool AbletonXsensAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
+#if JucePlugin_IsMidiEffect
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 double AbletonXsensAudioProcessor::getTailLengthSeconds() const
@@ -199,21 +209,21 @@ int AbletonXsensAudioProcessor::getCurrentProgram()
     return 0;
 }
 
-void AbletonXsensAudioProcessor::setCurrentProgram (int index)
+void AbletonXsensAudioProcessor::setCurrentProgram(int index)
 {
 }
 
-const juce::String AbletonXsensAudioProcessor::getProgramName (int index)
+const juce::String AbletonXsensAudioProcessor::getProgramName(int index)
 {
     return {};
 }
 
-void AbletonXsensAudioProcessor::changeProgramName (int index, const juce::String& newName)
+void AbletonXsensAudioProcessor::changeProgramName(int index, const juce::String& newName)
 {
 }
 
 //==============================================================================
-void AbletonXsensAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void AbletonXsensAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
     // Use this method as the place to do any pre-playback
     // initialisation that you need..
@@ -226,35 +236,35 @@ void AbletonXsensAudioProcessor::releaseResources()
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool AbletonXsensAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+bool AbletonXsensAudioProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
 {
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
+#if JucePlugin_IsMidiEffect
+    juce::ignoreUnused(layouts);
     return true;
-  #else
+#else
     // This is the place where you check if the layout is supported.
     // In this template code we only support mono or stereo.
     // Some plugin hosts, such as certain GarageBand versions, will only
     // load plugins that support stereo bus layouts.
     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
     // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
+#if ! JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
-   #endif
+#endif
 
     return true;
-  #endif
+#endif
 }
 #endif
 
-void AbletonXsensAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+void AbletonXsensAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
     juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumInputChannels = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
     // In case we have more outputs than inputs, this code clears any output
@@ -264,7 +274,7 @@ void AbletonXsensAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     // when they first compile a plugin, but obviously you don't need to keep
     // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
+        buffer.clear(i, 0, buffer.getNumSamples());
 }
 
 //==============================================================================
@@ -275,18 +285,18 @@ bool AbletonXsensAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* AbletonXsensAudioProcessor::createEditor()
 {
-    return new AbletonXsensAudioProcessorEditor (*this);
+    return new AbletonXsensAudioProcessorEditor(*this);
 }
 
 //==============================================================================
-void AbletonXsensAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+void AbletonXsensAudioProcessor::getStateInformation(juce::MemoryBlock& destData)
 {
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
 }
 
-void AbletonXsensAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void AbletonXsensAudioProcessor::setStateInformation(const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -15,15 +15,18 @@
 
 #define PARAMS_NUM 3
 #define SENSITIVITY "_sensitivity"
-#define MIN_SENSITIVITY 0
-#define MAX_SENSITIVITY 10
+#define MIN_SENSITIVITY 1
+#define MAX_SENSITIVITY 3
 #define INVERTION "_invertion"
-#define MAX_SENSORS_NUM 5
+#define SHIFTING "_shifting"
+#define MIN_SHIFTING -3.14
+#define MAX_SHIFTING 3.14
+#define MAX_SENSORS_NUM 4
 
 //==============================================================================
 /**
 */
-class AbletonXsensAudioProcessor  : public juce::AudioProcessor
+class AbletonXsensAudioProcessor : public juce::AudioProcessor
 {
 public:
     juce::AudioProcessorValueTreeState treeState;
@@ -36,14 +39,14 @@ public:
     ~AbletonXsensAudioProcessor() override;
 
     //==============================================================================
-    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
     void releaseResources() override;
 
-   #ifndef JucePlugin_PreferredChannelConfigurations
-    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
-   #endif
+#ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported(const BusesLayout& layouts) const override;
+#endif
 
-    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+    void processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
 
     //==============================================================================
     juce::AudioProcessorEditor* createEditor() override;
@@ -60,13 +63,13 @@ public:
     //==============================================================================
     int getNumPrograms() override;
     int getCurrentProgram() override;
-    void setCurrentProgram (int index) override;
-    const juce::String getProgramName (int index) override;
-    void changeProgramName (int index, const juce::String& newName) override;
+    void setCurrentProgram(int index) override;
+    const juce::String getProgramName(int index) override;
+    void changeProgramName(int index, const juce::String& newName) override;
 
     //==============================================================================
-    void getStateInformation (juce::MemoryBlock& destData) override;
-    void setStateInformation (const void* data, int sizeInBytes) override;
+    void getStateInformation(juce::MemoryBlock& destData) override;
+    void setStateInformation(const void* data, int sizeInBytes) override;
 
     void onDataTransfer(std::string msg);
     int extractSlot(std::string firstLine);
@@ -77,8 +80,9 @@ private:
     juce::FileLogger m_logger;
     StreamAllocator streamAllocator;
     juce::AudioProcessorValueTreeState::ParameterLayout createParameters();
+    float AbletonXsensAudioProcessor::mapSensorValues(std::string currParam, double paramValue);
     //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AbletonXsensAudioProcessor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AbletonXsensAudioProcessor)
 
 };
 
